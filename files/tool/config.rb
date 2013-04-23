9cb3a5fe991480e1e0faa7d4a516fe7bd990698a
t32k <ijok.ijok@gmail.com>
2013-04-23 18:42:10 +0900

    Fix base.scss

--- files/tool/config.rb
+++ files/tool/config.rb
@@ -16,7 +16,7 @@ output_style = :compact
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = true
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
