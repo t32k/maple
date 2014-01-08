c266cb21e104a00e981ba94f373a982c69811745
Koji Ishimoto <ijok.ijok@gmail.com>
2014-01-08 10:00:28 +0900

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,1 @@
-webpagetest test http://t32k.me/maple/test/fixtures/perf_test.html --server $1 --poll 3 --specs spec.json --reporter xunit > webpagetest.xml
+webpagetest test http://t32k.me/maple/test/fixtures/perf_test.html --server $1 --poll 3 --specs test/spec.json --reporter xunit > webpagetest.xml
