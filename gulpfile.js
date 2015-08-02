7fae1ae24d66d453158d02dfc52370c3f9284acb
t32k <ijok.ijok@gmail.com>
2015-08-02 21:27:42 +0900

    Update cssnext options

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,13 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
 var cssnext = require("gulp-cssnext");
+var browserSync = require('browser-sync').create();
 
-
-// Static server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-});
-
-gulp.task("css", function() {
-  gulp.src("assets/css/maple.css")
+// cssnext
+gulp.task("css", function () {
+  gulp.src('assets/css/maple.css')
     .pipe(cssnext({
+      browsers: ['last 2 versions'],
       compress: false
     }))
-    .pipe(gulp.dest("public/files/css/"));
+    .pipe(gulp.dest('public/files/css/'));
 });
-
-gulp.task("csss", function () {
-
-  var postcss = require("gulp-postcss")
-  var stylelint = require("stylelint")
-  var reporter = require("postcss-reporter")
-
-  return gulp.src("src/**/*.css")
-    .pipe(postcss([
-      stylelint({ // an example config that has four rules
-        "rules": {
-          "color-no-invalid-hex": 2,
-          "declaration-colon-space-before": [2, "never"],
-          "indentation": [2, "tab"],
-          "number-leading-zero": [2, "always"]
-        }
-      }),
-      reporter({
-        clearMessages: true,
-      })
-    ]))
-})
