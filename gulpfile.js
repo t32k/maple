d92dde93e20b013d24b41a181be0f784664d6d30
t32k <ijok.ijok@gmail.com>
2015-08-04 15:31:39 +0900

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-var cssnext = require("gulp-cssnext");
+var cssnext = require('gulp-cssnext');
 var browserSync = require('browser-sync').create();
-
-var postcss = require("gulp-postcss")
-var stylelint = require("stylelint")
-var reporter = require("postcss-reporter")
+var postcss = require('gulp-postcss');
+var stylelint = require('stylelint');
+var reporter = require('postcss-reporter');
 
 
 gulp.task('css', function () {
-  gulp.src('css/maple.css')
-    .pipe(cssnext({
-      browsers: ['last 2 versions'],
-      compress: false
-    }))
-    .pipe(gulp.dest('public/files/css/'));
+    gulp.src('css/maple.css')
+        .pipe(cssnext({
+            browsers: ['last 2 versions'],
+            compress: false
+        }))
+        .pipe(gulp.dest('public/files/css/'));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('/css/**/*.css', ['css']);
+gulp.task('serve', function () {
+    browserSync.init({
+        server: "public/"
+    });
+    gulp.watch('/css/**/*.css', ['css']);
+    gulp.watch('public/**/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve']);
