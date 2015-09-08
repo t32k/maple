9ab74f2ccac88207ca5d01c656d80e4fc3116191
t32k <ijok.ijok@gmail.com>
2015-09-08 21:22:36 +0900

    Add stylelint

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ This is alpha version, yet.
 
 ## Features
 
+Require [PostCSS](https://github.com/postcss/postcss).
+
+### Plugins
+
++ 
++ [cssnano](https://github.com/ben-eb/cssnano)
+
 ## Installation
 
 This project use a bunch of npm, so you need [Node.js](https://nodejs.org/) v0.12+.
--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,37 @@ var customProps = require('postcss-custom-properties');
 var calc = require('postcss-calc');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
-
+var stylelint = require('stylelint');
+var reporter = require('postcss-reporter');
 
 gulp.task('css', function () {
-    var processors = [
-        atImport(),
-        customProps(),
-        calc(),
-        autoprefixer({browsers: ['last 2 version']}),
-        cssnano()
-    ];
-    return gulp.src('./src/css/*.css')
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./dest/css'));
+  var processors = [
+    atImport(),
+    customProps(),
+    calc(),
+    autoprefixer({browsers: ['last 2 version']}),
+    cssnano(),
+    stylelint({
+      "rules": {
+        "string-quotes": [2,"double"]
+      }
+    }),
+    reporter({
+      clearMessages: true
+    })
+  ];
+  return gulp.src('./src/css/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dest/css'));
 });
 
 gulp.task('serve', function () {
-    browserSync.init({
-        server: 'dest/',
-        open: false
-    });
-    gulp.watch('src/css/**/*.css', ['css']);
-    gulp.watch('dest/**/*.html').on('change', browserSync.reload);
+  browserSync.init({
+    server: 'dest/',
+    open: false
+  });
+  gulp.watch('src/css/**/*.css', ['css']);
+  gulp.watch('dest/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
