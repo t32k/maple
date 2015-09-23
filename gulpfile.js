116d3e64845d7d7f2f32e7d511cd65f1dfd24694
t32k <ijok.ijok@gmail.com>
2015-09-24 00:01:51 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,26 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var postcss = require('gulp-postcss');
-var atImport = require('postcss-import');
-var customProps = require('postcss-custom-properties');
-var calc = require('postcss-calc');
-var autoprefixer = require('autoprefixer');
-var cssnano = require('cssnano');
-var stylelint = require('stylelint');
-var reporter = require('postcss-reporter');
+var processors = [
+  require('postcss-import'),
+  require('postcss-custom-properties'),
+  require('postcss-calc'),
+  require('autoprefixer')({browsers: ['last 2 version']}),
+  require('cssnano'),
+  require('stylelint')({
+    "rules": {
+      "string-quotes": [2, "double"],
+      "block-no-empty": 2,
+      "color-hex-case": [2, "lower"],
+      "selector-no-id": 2
+    }
+  }),
+  require('postcss-reporter')({
+    clearMessages: true
+  })
+];
 
 gulp.task('css', function () {
-  var processors = [
-    atImport(),
-    customProps(),
-    calc(),
-    autoprefixer({browsers: ['last 2 version']}),
-    cssnano(),
-    stylelint({
-      "rules": {
-        "string-quotes": [2, "double"],
-        "block-no-empty": 2,
-        "color-hex-case": [2, "lower"],
-        "selector-no-id": 2
-      }
-    }),
-    reporter({
-      clearMessages: true
-    })
-  ];
   return gulp.src('./src/css/*.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dest/css'));
