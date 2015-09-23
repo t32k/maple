00e0d3b687acb0d349cfc4bf56089f515d27d491
t32k <ijok.ijok@gmail.com>
2015-09-24 00:25:07 +0900

    Add babel plugin

---
+++ dest/js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var log = function log(message) {
+  return console.log(message);
+};
+
+log('Hello Babel!');
+//# sourceMappingURL=app.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var postcss = require('gulp-postcss');
 var processors = [
   require('postcss-import'),
   require('postcss-custom-properties'),
@@ -9,11 +11,11 @@ var processors = [
   require('autoprefixer')({browsers: ['last 2 version']}),
   require('cssnano'),
   require('stylelint')({
-    "rules": {
-      "string-quotes": [2, "double"],
-      "block-no-empty": 2,
-      "color-hex-case": [2, "lower"],
-      "selector-no-id": 2
+    'rules': {
+      'string-quotes': [2, 'double'],
+      'block-no-empty': 2,
+      'color-hex-case': [2, 'lower'],
+      'selector-no-id': 2
     }
   }),
   require('postcss-reporter')({
@@ -21,12 +23,21 @@ var processors = [
   })
 ];
 
+gulp.task('js', function () {
+  return gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat('app.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dest/js'));
+});
+
 gulp.task('css', function () {
-  return gulp.src('./src/css/*.css')
+  return gulp.src('src/**/*.css')
     .pipe(sourcemaps.init())
     .pipe(postcss(processors))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dest/css'));
+    .pipe(gulp.dest('dest/css'));
 });
 
 gulp.task('serve', function () {
@@ -34,8 +45,10 @@ gulp.task('serve', function () {
     server: 'dest/',
     open: false
   });
-  gulp.watch('src/css/**/*.css', ['css']);
+  gulp.watch('src/**/*.js', ['js']);
+  gulp.watch('src/**/*.css', ['css']);
   gulp.watch('dest/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
+gulp.task('build', ['js', 'css']);
