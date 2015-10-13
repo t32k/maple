1525c6b262cbf669ef784853d68029f3a97613f3
t32k <ijok.ijok@gmail.com>
2015-10-14 00:18:10 +0900

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ var processors = [
 
 var browserify = require('browserify');
 var babelify = require('babelify');
+var watchify = require('watchify');
 var uglify = require('gulp-uglify');
 
 // Clean Dir
